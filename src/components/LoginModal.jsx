7cb63d787c06d979b24c73e32e26ba3eddc02bde
AESalva <eduardo_salva@hotmail.com>
2022-10-29 20:03:50 -0300

    register login passrecovery ready to visual test

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -4,6 +4,7 @@ import Modal from "react-bootstrap/Modal";
 import Form from 'react-bootstrap/Form';
 import ModalDialog from 'react-bootstrap/ModalDialog'
 import {Link, useNavigate} from "react-router-dom";
+import { NavLink } from "react-bootstrap";
 
 
 
@@ -17,7 +18,13 @@ const LoginModal = (auth,
   const [name, setName] = useState("");
   const [pass, setPass] = useState("");
   const navigate = useNavigate();
-
+  const handleValidation = (e) => {
+    e.preventDefault();
+    if (validate(name, pass)) {
+      login(name);
+      navigate("/");
+    }
+  };
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -33,29 +40,29 @@ const LoginModal = (auth,
         show={show}
         onHide={handleClose}
         keyboard={false}
-        contentClassName="bg-dark"
+        contentClassName="bg-dark "
       >
         <Modal.Header className="border-danger bg-dark">
-          <img className="mx-auto bg-dark logo" src="/img/LogoJS.jpg"width="100"height="100"/>
+          <Link className="mx-auto" to="/"><img className="bg-dark logo" src="/img/LogoJS.jpg"width="100"height="100"/></Link>
         </Modal.Header>
-        <h2 className="mx-auto mt-5">Iniciar Sesión</h2>
-        <Modal.Body className="mx-auto bg-dark">¿ No tienes cuenta ? Registráte</Modal.Body>
+        <h2 className="mx-auto mt-4">Iniciar Sesión</h2>
+        <Modal.Body className="mx-auto bg-dark">¿ No tienes cuenta ? <Link className="text-danger modal_styles" to="/Register" >Registráte</Link> </Modal.Body>
         <Modal.Body className="mb-3 py-4 px-4 bg-dark">
         <Form>
       <Form.Group className="mb-3 bg-dark" controlId="formBasicEmail">
         <Form.Label>Usuario</Form.Label>
-        <Form.Control value={name} className="p-3" type="text" placeholder="JavaSports" />
+        <Form.Control value={name} onInput={(e) => setName(e.target.value)} className="p-3" type="text" placeholder="JavaSports" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Contraseña</Form.Label>
-        <Form.Control value={pass} className="p-3" type="password" placeholder="***** JavaSports *****" />
+        <Form.Control value={pass} onInput={(e) => setPass(e.target.value)} className="p-3" type="password" placeholder="***** JavaSports *****" />
       </Form.Group>
       <Form.Text className="text-danger">
-        Olvidé mi contraseña
+        <Link to="/PassRecovery" className="text-danger modal_styles" >Olvidé mi contraseña</Link>
         </Form.Text>
     </Form>
         </Modal.Body>
-          <Button className="m-auto px-5 mb-5" size="lg" variant="danger" onClick={handleClose}>
+          <Button className="m-auto px-5 mb-5" size="lg"  type="submit" variant="danger" onClick={(e) => handleValidation(e)}>
             <h4 className="m-auto py-1 px-4">Iniciar</h4>
           </Button>
         
--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import { Link, Route, Routes } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Landing from '../components/Landing'
+import Aboutus from '../components/Aboutus'
+import Register from '../components/Register'
+import PassRecovery from '../components/PassRecovery';
 
 const Main = (auth,
   login,
@@ -9,13 +13,18 @@ const Main = (auth,
   validate,
   setAuth) => {
   return (
-    <Container fluid className="min-vh-100 d-flex ">
-    <Landing login={login}
+    <Container fluid className="min-vh-100 d-flex flex-column">
+      <Routes>
+        <Route element={<Aboutus />} path="/Aboutus"></Route>
+    <Route element={<Landing login={login}
         logout={logout}
         setAuth={setAuth}
         validate={validate}
         auth={auth}
-        />
+        />} path="/" ></Route>
+        <Route element={<Register />} path="/Register"></Route>
+        <Route element={<PassRecovery />} path="/PassRecovery"></Route>
+        </Routes>
     <Footer />
     </Container>
   )
