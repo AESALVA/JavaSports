fa0e18bd1b7f7281391f5553fadd0df90e94f977
Valentina Reigada <valentinareigada18@gmail.com>
2022-10-30 17:00:49 -0300

    Agrego archivo Header.jsx

--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import Footer from './Footer'
-import Landing from './Landing'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Footer from "./Footer";
+import Header from "./Header";
+import Landing from "./Landing";
 
 const Main = () => {
   return (
     <Container fluid className="min-vh-100 d-flex ">
-    <Landing />
-    <Footer />
+      <Header />
+      <Landing />
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
