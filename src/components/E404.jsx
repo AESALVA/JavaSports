cfbac675d3f14cdd743f13d2519dafbaaf0b9f57
JoaquinRostan <joaquinrostan@gmail.com>
2022-11-07 22:06:40 -0300

    E404 Primera version

---
+++ src/components/E404.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+function E404(){
+    return(
+        <div>
+             <div id="main404">
+                    <div class="container404">
+                            <h1>Error 404</h1>
+                    </div>
+            </div>
+        </div>
+    )
+}
+export default E404;
