8ac4d1bc07c6a9e0826be4e5c505bf0edc9644c2
JoaquinRostan <joaquinrostan@gmail.com>
2022-11-10 20:29:07 -0300

    creacion nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import  Footer from './components/Footer.jsx';
 import E404 from './components/E404';
+import Nosotros from './components/Nosotros';
 
 
 function App() {
   return (
     <>
      <E404/> 
+     <Nosotros/>
      <Footer/> 
     </>
   );
