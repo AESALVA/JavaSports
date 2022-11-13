df337412542975680e611fbc253d811f785dfbdb
JoaquinRostan <joaquinrostan@gmail.com>
2022-11-13 20:55:08 -0300

    Aboutus

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,17 +5,25 @@ function Aboutus (){
     <div className='container-aboutus'>
       <div className='main-aboutus'>
         <div className='d-flex justify-content-center mt-4'>
-          <h1>Sobre Nosotros:</h1>
+          <h1 className='titulo-aboutus '>Sobre Nosotros:</h1>
         </div>
-        <div className='Eduardo d-flex justify-content-center'>
-          <div className='d-flex'><h2>Eduardo</h2></div>
-          <img src="../img/imgSection.webp" alt="" />
+
+        <div className='integrante'>
+          <div className='d-flex justify-content-center my-3'><h2>Eduardo</h2></div>
+          <div className='d-flex justify-content-center'><img src="../img/eduardo.jpeg" width={250} height={250} className='rounded-circle my-3 d-flex justify-content-center' alt="" /></div>
+          <p className='mx-5 my-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quasi obcaecati asperiores, ad eveniet libero voluptate, omnis eum, iste laboriosam sapiente a! Neque, dolore. Rerum non laborum in explicabo ipsa?</p>
         </div>
-        <div className='Valentina d-flex justify-content-center'>
-          <h2>Valentina</h2>
+        
+        <div className='integrante'>
+          <div className='d-flex justify-content-center my-3'><h2>Valentina</h2></div>
+          <div className='d-flex justify-content-center'><img src="../img/valentina.jpeg" width={250} height={250} className='rounded-circle my-3 d-flex justify-content-center' alt="" /></div>
+          <p className='mx-5 my-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quasi obcaecati asperiores, ad eveniet libero voluptate, omnis eum, iste laboriosam sapiente a! Neque, dolore. Rerum non laborum in explicabo ipsa?</p>
         </div>
-        <div className='Joaquin d-flex justify-content-center'>
-          <h2>Joaquin</h2>
+
+        <div className=''>
+          <div className='d-flex justify-content-center my-3'><h2>Joaquin</h2></div>
+          <div className='d-flex justify-content-center'><img src="../img/joaquin.jpeg" width={250} height={250} className='rounded-circle my-3 d-flex justify-content-center' alt="" /></div>
+          <p className='mx-5 my-4'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quasi obcaecati asperiores, ad eveniet libero voluptate, omnis eum, iste laboriosam sapiente a! Neque, dolore. Rerum non laborum in explicabo ipsa?</p>
         </div>
       </div>
     </div>
