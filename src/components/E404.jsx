4dfdcac491c1c880b236d2e5c3e4c076d7abd92c
Valentina Reigada <valentinareigada18@gmail.com>
2022-11-23 20:22:24 -0300

    correccion en pagina error 404

--- src/components/E404.jsx
+++ src/components/E404.jsx
@@ -5,35 +5,19 @@ import "../styles/joaquin.css";
 function E404() {
   return (
     <div className="container-400">
-      <div>
+      <div className="d-flex justify-content-center  container404 ">
         <h1>Error 404</h1>
       </div>
       <div id="main404">
-        {/* <img src="/src/img/arbitro.png" alt="arbitro" /> */}
-      </div>
-      <div>
-        <Link to="/">
-          <Button id="btn404" variant="light" className="">
-            Volver al home
-          </Button>
-        </Link>
+        <div className="mb-5">
+          <Link to="/">
+            <Button id="btn404" className="btn-light">
+              Volver al home
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
 export default E404;
-
-// <div id="main404">
-// <div className="container404 d-flex justify-content-center">
-//   <h1>Error 404</h1>
-//   <Link to="/">
-//     <Button
-//       id="btn404"
-//       variant="light"
-//       className="d-flex justify-content-center align-items-center"
-//     >
-//       Volver al home
-//     </Button>
-//   </Link>
-// </div>
-// </div>
