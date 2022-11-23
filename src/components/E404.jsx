1a97bcbf4fde0430b07210f536797fc4fa161635
Valentina Reigada <valentinareigada18@gmail.com>
2022-11-23 00:24:37 -0300

    correccion en pagina 404

--- src/components/E404.jsx
+++ src/components/E404.jsx
@@ -1,22 +1,27 @@
-import React from "react";
 import { Button } from "react-bootstrap";
-import '../App.css';
+import { Link } from "react-router-dom";
+// import "../App.css";
+import "../styles/joaquin.css";
 
-
-function E404(){
-    return(
-        <div className="containerE404">
-             <div id="main404">
-                
-                    <div className="container404">
-                            <h1>Error 404</h1>
-                    </div>
-                   
-            </div>
-            <div className="d-flex align-items-center justify-content-center py-4">
-                      <Button id="btn404" variant="light" className="d-flex justify-content-center align-items-center"> Volver al home</Button>
-                    </div>
-        </div>
-    )
+function E404() {
+  return (
+    <div className="container mb-5">
+      <div className="container404 d-flex justify-content-center">
+        <h1>Error 404</h1>
+      </div>
+      <div id="main404">
+        <Link to="/">
+          <Button
+            id="btn404"
+            variant="light"
+            className="d-flex justify-content-center align-items-center"
+          >
+            {" "}
+            Volver al home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 export default E404;
