6779f0bd36d7acedd99fcc93035641a20c3e15df
AESalva <eduardo_salva@hotmail.com>
2022-11-28 22:44:57 -0300

    delete button on comentaries

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
     fetch("https://java-sports-back.vercel.app/comments/all")
       .then((res) => res.json())
       .then((json) => setCommentsContainer(json));
-  }, []);
+  }, [commentsContainer]);
 
   // USUARIOS
 
--- src/components/LikeCounter.jsx
+++ src/components/LikeCounter.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import Button from "react-bootstrap/Button";
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 
 
@@ -15,9 +15,18 @@ const addCounter = (c, q) => {
     addLikes(comment);
     console.log(comment.likes.length)
   };
+
+  const deleteComment = (id)=>{
+    fetch(`https://java-sports-back.vercel.app/comments/delete/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+    console.log(id)
+  }
  
   return (
     <div><Button disabled={comment.likes.find(c=>c===auth.user)} variant="secondary" onClick={() => addCounter(counter, 1)} className="border-0">{comment.likes.length>0 && comment.likes.length}{' '}<FontAwesomeIcon icon={faThumbsUp} /></Button>
+    {' '}{auth.role === "admin" && <Button variant='secondary' onClick={()=>deleteComment(comment._id) } ><FontAwesomeIcon icon={faTrashCan} /></Button>}
     </div>
   )
 }
