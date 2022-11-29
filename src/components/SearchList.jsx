e4a05d1c05e628d03139d12aed062e9a6308de8f
Valentina Reigada <valentinareigada18@gmail.com>
2022-11-29 20:39:06 -0300

    correccion en pagina sekeer

--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -7,8 +7,8 @@ const SearchList = ({ matchList, auth }) => {
   return (
     <Container className="my-5">
       <Row className="" md={3} xs={12}>
-        {matchList.map((item) => (
-          <div className="spaceNews">
+        {matchList.map((item, i) => (
+          <div className="spaceNews" key={i}>
             <News sizeCard="card-big" view="Home" article={item} auth={auth} />
           </div>
         ))}
