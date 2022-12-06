da4f21399dd23c259a50273c63d0bc796d53744d
Valentina Reigada <valentinareigada18@gmail.com>
2022-12-05 23:04:27 -0300

    Correcciones en página de contacto

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
     fetch("https://java-sports-back.vercel.app/articles/all")
       .then((res) => res.json())
       .then((json) => setArticles(json));
-  }, [articles]);
+  }, []);
 
   //COMENTARIOS
 
@@ -64,7 +64,7 @@ function App() {
     fetch("https://java-sports-back.vercel.app/comments/all")
       .then((res) => res.json())
       .then((json) => setCommentsContainer(json));
-  }, [commentsContainer]);
+  }, []);
 
   // USUARIOS
 
