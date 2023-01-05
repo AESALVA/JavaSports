48b31e685cdc5b35f05f3cde94d02f2c5481486a
AESalva <eduardo_salva@hotmail.com>
2023-01-05 19:30:53 -0300

    inputs validate ok register pass validation ok

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -50,7 +50,12 @@ const Register = ({ setAuth, addUser }) => {
   const validatePass = (n) => {
     return (
       validator.matches(n, "^[a-zA-Z0-9]*$") &&
-      validator.isLength(n, { min: 8, max: 20 })
+      validator.isLength(n, { min: 8, max: 20 }) && 
+      validator.isStrongPassword(n,{ minLength: 8, minLowercase: 1, minUppercase: 1, 
+        minNumbers: 1, minSymbols: 0, returnScore: false, 
+        pointsPerUnique: 1, pointsPerRepeat: 0.5, 
+        pointsForContainingLower: 10, pointsForContainingUpper: 10, 
+        pointsForContainingNumber: 10, pointsForContainingSymbol: 10 })
     );
   };
 
--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,15 +1,14 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "../styles/resetPassword.css";
 import Container from "react-bootstrap/Container";
-import { useState } from 'react';
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import validator from "validator";
+import { useEffect } from "react";
 
-
-
-function ResetPassword  () {
+function ResetPassword() {
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
   const [token, setToken] = useState("");
@@ -21,58 +20,92 @@ function ResetPassword  () {
 
   const validatePassword = (n) => {
     return (
-      validator.isLength(n, { min: 8, max: 20 }) &&
-      validator.isStrongPassword(n, {
-        minLowercase: 1,
-        minUppercase: 1,
-        minNumbers: 1,
-      })
+      validator.matches(n, "^[a-zA-Z0-9]*$") &&
+      validator.isLength(n, { min: 8, max: 20 })
     );
   };
 
+  const validateToken = (n) => {
+    return (
+      validator.matches(n, "^[a-zA-Z0-9]*$") &&
+      validator.isLength(n, { min: 8, max: 50 }) &&
+      validator.isStrongPassword(n,{ minLength: 8, minLowercase: 1, minUppercase: 0, 
+        minNumbers: 1, minSymbols: 0, returnScore: false, 
+        pointsPerUnique: 1, pointsPerRepeat: 0.5, 
+        pointsForContainingLower: 10, pointsForContainingUpper: 10, 
+        pointsForContainingNumber: 10, pointsForContainingSymbol: 10 })
+    );
+  };
 
+  const HandleClick = ()=>{
+if(validatePassword(password) && validatePassword(repeatPassword) && validateToken(token) ){
+  console.log("OK")
+}else{
+  console.log("error")
+}
+    
+  }
 
-
-
-  
   return (
-      <Container className="resetContainer p-5">
-        <h2>Formulario para restablecer contraseña </h2>
-        <p>La nueva contraseña debe contar con al menos :</p>
-        <ul>
-          <li> Una letra mayúscula.</li>
-          <li> Una letra minúscula.</li>
-          <li>Tener entre 8 y 20 caracteres.</li>
-        </ul>
-        <div className='inputs'>
-          <FloatingLabel
-            controlId="floatingInput" label="Nueva Contraseña">
-            <Form.Control type="text" placeholder="Contraseña" maxLength="20" required  value={password}
-                onChange={(e) => setPassword(e.target.value)} onBlur={() => setFirstPass(false)} />
-           <span className='message'>{message}</span>
-          </FloatingLabel>
-          <FloatingLabel controlId="floatingPassword" label="Repetir Contraseña">
-            <Form.Control type="text" placeholder="Repetir Contraseña" maxLength="20" required value={repeatPassword}
-                onChange={(e) => setRepeatPassword(e.target.value)} onBlur={() => setSecondPass(false)} />
-            <span className='message'>{message}</span>
-          </FloatingLabel>
-          <FloatingLabel controlId="floatingToken" label="Clave Token">
-            <Form.Control type="text" placeholder="Clave Token" maxLength="60" required value={token}
-                onChange={(e) => setToken(e.target.value)} onBlur={() => setFirstToken(false)} />
-            <span className='message'>{message}</span>
-          </FloatingLabel>
-          </div>
-          <Button className='btnConfirm m-5 mx-auto' onClick={()=>console.log("Enviado")} size='lg' >
-            Enviar
-          </Button>
-        </Container>
-      )
-    }
-  
+    <Container className="resetContainer p-5">
+      <h2>Formulario para restablecer contraseña </h2>
+      <p>La nueva contraseña debe contar con al menos :</p>
+      <ul>
+        <li> Una letra mayúscula.</li>
+        <li> Una letra minúscula.</li>
+        <li>Tener entre 8 y 20 caracteres.</li>
+      </ul>
+      <div className="inputs">
+        <FloatingLabel controlId="floatingInput" label="Nueva Contraseña">
+          <Form.Control
+            type="text"
+            placeholder="Contraseña"
+            maxLength="20"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onBlur={() => setFirstPass(false)}
+          />
+          {!validatePassword(password) && !firstPass && (
+            <span className="message">Contraseña incorrecta</span>
+          )}
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Repetir Contraseña">
+          <Form.Control
+            type="text"
+            placeholder="Repetir Contraseña"
+            maxLength="20"
+            required
+            value={repeatPassword}
+            onChange={(e) => setRepeatPassword(e.target.value)}
+            onBlur={() => setSecondPass(false)}
+          />
+          {password !== repeatPassword && !secondPass && (
+            <span className="message">Las contraseñas deben coincidir</span>
+          )}
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingToken" label="Clave Token">
+          <Form.Control
+            type="text"
+            placeholder="Clave Token"
+            maxLength="60"
+            required
+            value={token}
+            onChange={(e) => setToken(e.target.value)}
+            onBlur={() => setFirstToken(false)}
+          />
+          {!validateToken(token) && !firstToken && (<span className="message">Clave Token incorrecta</span>)}
+        </FloatingLabel>
+      </div>
+      <Button
+        className="btnConfirm m-5 mx-auto"
+        onClick={() => HandleClick()}
+        size="lg"
+      >
+        Enviar
+      </Button>
+    </Container>
+  );
+}
 
-
-
-
-
-
-export default ResetPassword
+export default ResetPassword;
