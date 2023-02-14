79e4571c45c4a738225548aaf1a59a5917eeb490
AESalva <eduardo_salva@hotmail.com>
2023-02-14 17:25:56 -0300

    new features about likes on comments

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -13,33 +13,63 @@ import validator from "validator";
 import { useEffect } from "react";
 
 const Sections = ({ article, auth, commentsContainer }) => {
-  const [comment, setComment] = useState({ comment: "", user: "", likes: [], id:"" });
+  const [comment, setComment] = useState({
+    comment: "",
+    user: "",
+    likes: [],
+    id: "",
+  });
   const [showComment, setShowComment] = useState([]);
 
-   const addLikes = (comment) => {
-    
+  const addLikes = (comment) => {
     const aux = [...showComment];
     aux.map((a) => {
       if (a.comment === comment.comment) {
-        a.likes = [...a.likes, auth.user];  
-        fetch("https://java-sports-back.vercel.app/comments/update/" + a._id, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            comment: a.comment,
-            user: a.user,
-            likes: a.likes,
-          }),
-        }) 
-      } return a
-     
-     });
+        a.likes = [...a.likes, auth.user];
+        if (!a._id) {
+          fetch(`https://java-sports-back.vercel.app/comments/search`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(a),
+          })
+            .then((res) => res.json())
+            .then((json) =>
+              fetch(
+                "https://java-sports-back.vercel.app/comments/update/" + json,
+                {
+                  method: "PUT",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify({
+                    comment: a.comment,
+                    user: a.user,
+                    likes: a.likes,
+                  }),
+                }
+              )
+            );
+        } else {
+          fetch(
+            "https://java-sports-back.vercel.app/comments/update/" + a._id,
+            {
+              method: "PUT",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                comment: a.comment,
+                user: a.user,
+                likes: a.likes,
+              }),
+            }
+          );
+        }
+      }
+      return a;
+    });
     setShowComment([...aux]);
   };
 
   const addComment = () => {
     setShowComment([...showComment, comment]);
-    setComment({ comment: "", user: auth.user, likes: [], id:article._id });
+    setComment({ comment: "", user: auth.user, likes: [], id: article._id });
 
     fetch("https://java-sports-back.vercel.app/comments/newComment", {
       method: "POST",
@@ -59,8 +89,6 @@ const Sections = ({ article, auth, commentsContainer }) => {
     setShowComment(commentsContainer);
   }, [commentsContainer]);
 
- 
-
   return (
     <Container className="sections py-5">
       <Card className="mx-auto text-center sections border-0 ">
@@ -76,7 +104,9 @@ const Sections = ({ article, auth, commentsContainer }) => {
       <div className="commentArea-container sections">
         <h4 className="comments-title">
           All Comments {""}
-          {showComment.filter(comment=>comment.id===article._id).length > 0 && showComment.filter(comment=>comment.id===article._id).length}
+          {showComment.filter((comment) => comment.id === article._id).length >
+            0 &&
+            showComment.filter((comment) => comment.id === article._id).length}
         </h4>
         <input
           className="w-100 sections input-comments mt-3"
@@ -85,7 +115,12 @@ const Sections = ({ article, auth, commentsContainer }) => {
           type="text"
           value={comment.comment}
           onChange={(e) =>
-            setComment({ comment: e.target.value, user: auth.user, likes: [],id:article._id })
+            setComment({
+              comment: e.target.value,
+              user: auth.user,
+              likes: [],
+              id: article._id,
+            })
           }
         />
         <Button
@@ -95,18 +130,26 @@ const Sections = ({ article, auth, commentsContainer }) => {
         >
           <FontAwesomeIcon icon={faComment} /> Comentar
         </Button>
-        {showComment.map((comment, i) => (
-          comment.id === article._id &&
-          <Row key={i} className="p-4">
-            <Col>
-              <FontAwesomeIcon icon={faUser} /> ({comment.user})
-            </Col>
-            <span> {comment.comment}</span>
-            <span>
-              <LikeCounter addLikes={addLikes} comment={comment} auth={auth} setShowComment={setShowComment} showComment={showComment}/>
-            </span>
-          </Row>
-        ))}
+        {showComment.map(
+          (comment, i) =>
+            comment.id === article._id && (
+              <Row key={i} className="p-4">
+                <Col>
+                  <FontAwesomeIcon icon={faUser} /> ({comment.user})
+                </Col>
+                <span> {comment.comment}</span>
+                <span>
+                  <LikeCounter
+                    addLikes={addLikes}
+                    comment={comment}
+                    auth={auth}
+                    setShowComment={setShowComment}
+                    showComment={showComment}
+                  />
+                </span>
+              </Row>
+            )
+        )}
       </div>
     </Container>
   );
