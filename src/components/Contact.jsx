2de47de26a6b020ceb39dc7f3c5a8165fbc0818f
Valentina Reigada <valentinareigada18@gmail.com>
2023-02-22 23:12:14 -0300

    Correcció de ortografia

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -210,7 +210,7 @@ const Contacto = () => {
         </div>
         {/* MAPA */}
         <div className="container-fluid px-0 pb-5 pt-3">
-          <h4 className="titulo-aboutus text-center my-4">Nuestra ubicacion</h4>
+          <h4 className="titulo-aboutus text-center my-4">Nuestra ubicación</h4>
           <div className="container-fluid d-flex justify-content-center mb-5 ">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.102370777448!2d-65.20939048436118!3d-26.836696096503207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1669045414605!5m2!1ses-419!2sar"
